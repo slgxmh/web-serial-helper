c96a5a78512a25c71c9b6cabd9574307df9ff2d5
slgxmh <slgxmh@foxmail.com>
2025-09-27 23:33:46 +0800

    feat: ui

--- web/src/components/AppHeader.tsx
+++ web/src/components/AppHeader.tsx
@@ -9,12 +9,29 @@ export default function AppHeader() {
   };
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>{" "}
+          </svg>
+        </button>
+      </div>
+      <div className="flex-1">
         <a className="btn btn-ghost text-xl">{t("appName")}</a>
       </div>
-      <div className="navbar-center"></div>
-      <div className="navbar-end">
+      <div className="flex-none">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost">
             <Globe />
